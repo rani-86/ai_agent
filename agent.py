92d06b28a0f9f82b26d1d2a29b38ab30cdfcda2d
rani-86 <149258425+rani-86@users.noreply.github.com>
2026-08-01 01:01:18 +0530

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,15 +1,18 @@
+import os
 from openai import OpenAI
 from rag import retrieve
 from tools import get_order_status
 from memory import get_memory, update_memory
 
-client = OpenAI()
+client = OpenAI(
+    api_key=os.environ.get("GROQ_API_KEY"),
+    base_url="https://api.groq.com/openai/v1"
+)
 
 def agent(user_id, message):
     memory = get_memory(user_id)
     context = retrieve(message)
 
-    # tool detection (basic logic)
     if "order" in message:
         order_id = "".join(filter(str.isdigit, message))
         tool_result = get_order_status(order_id)
@@ -18,20 +21,16 @@ def agent(user_id, message):
 
     prompt = f"""
     You are an AI support agent.
-
     Context: {context}
     Memory: {memory}
     Tool Output: {tool_result}
-
     User: {message}
     """
 
     response = client.chat.completions.create(
-        model="gpt-4o-mini",
+        model="llama-3.3-70b-versatile",
         messages=[{"role": "user", "content": prompt}]
     )
-
     reply = response.choices[0].message.content
     update_memory(user_id, message, reply)
-
     return reply
